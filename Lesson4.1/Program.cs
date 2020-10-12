b0136e276cc0a891ffc3d212a3999513ce9b9798
SanchezV <Vechkunin.ru@mail.ru>
2020-10-12 22:13:58 +0400

    выполнил задания из 4 урока

---
+++ Lesson4.1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4._1
+{
+    public abstract class  AbstractHandler
+    {
+        public abstract void Open();
+        public abstract void Create();
+        public abstract void Chenge();
+        public abstract void Save();
+    }
+    public class XMLHandler : AbstractHandler
+    {
+        public override void Open()
+        {
+            Console.WriteLine("File.Open(\"Открыт файл XMLHandler\")");
+        }
+        public override void Create()
+        {
+            Console.WriteLine("File.Create(\"Path to XMLFile\")"); 
+        }
+        public override void Chenge()
+        {
+            Console.WriteLine("File.WriteAllText(\"Path to XMLFile\") for change"); 
+        }
+
+        public override void Save()
+        {
+            Console.WriteLine("File.WriteAllText(\"Path to XMLFile\") for save");
+        }
+    }
+    public class TXTHandler : AbstractHandler
+    {
+        public override void Open()
+        {
+            Console.WriteLine("File.Open(\"Открыт файл TXTHandler\")");
+        }
+        public override void Create()
+        {
+            Console.WriteLine("File.Create(\"Path to TXTFile\")");
+        }
+        public override void Chenge()
+        {
+            Console.WriteLine("File.WriteAllText(\"Path to TXTFile\") for change");
+        }
+
+        public override void Save()
+        {
+            Console.WriteLine("File.WriteAllText(\"Path to TXTFile\") for save");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AbstractHandler txt = new TXTHandler();
+            AbstractHandler xml = new XMLHandler();
+            txt.Create();
+            txt.Open();
+            txt.Chenge();
+            txt.Save();
+            xml.Create();
+            xml.Open();
+            xml.Chenge();
+            xml.Save();
+        }
+    }
+}
---
+++ Lesson4.2 Interface/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4._2_Interface
+{
+    class Program
+    {
+        interface IPlayable
+        {
+            void Play();
+            void Pause();
+            void Stop();
+        }
+        interface IRecordable
+        {
+            void Recod();
+            void Pause();
+            void Stop();
+        }
+        public class Player : IPlayable, IRecordable
+        {
+            void IPlayable.Play()
+            {
+                Console.WriteLine("Играет музыка!");
+            }
+            
+            void IPlayable.Pause()
+            {
+                Console.WriteLine("Музыка ставится на паузу!");
+            }
+
+
+            void IPlayable.Stop()
+            {
+                Console.WriteLine("Воспроизведение останавливается!");
+            }
+
+            void IRecordable.Recod()
+            {
+                Console.WriteLine("Идет запись!");
+            }
+
+            void IRecordable.Pause()
+            {
+                Console.WriteLine("Запись приостановлена!");
+            }
+
+            void IRecordable.Stop()
+            {
+                Console.WriteLine("Запись прекращена!");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Player myPlayer = new Player();
+            ((IRecordable)myPlayer).Recod();
+            ((IRecordable)myPlayer).Pause();
+            ((IRecordable)myPlayer).Stop();
+            ((IPlayable)myPlayer).Play();
+            ((IPlayable)myPlayer).Pause();
+            ((IPlayable)myPlayer).Stop();
+
+        }
+    }
+}
