5ae01fcfe5d8c63487b13709e809bb5ac45e51f0
SanchezV <Vechkunin.ru@mail.ru>
2020-12-03 23:34:59 +0400

    Сделал дополнительное задание из урока 10.

---
+++ Lesson10.1/Demonstration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson10._1
+{
+    class Demonstration
+    {
+        static void Main(string[] args)
+        {
+            int i = MyClass<int>.FactoryMethod();
+            Console.WriteLine(i.GetType().Name); //Метод отображает имя типа переменной i
+
+            Demonstration p = MyClass<Demonstration>.FactoryMethod(); //Создаем переменную типа Program
+
+            Console.WriteLine(p.GetType().Name); //Метод отображает имя типа переменной p
+
+        }
+    }
+}
---
+++ Lesson10.1/MyClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson10._1
+{
+    class MyClass<T> where T : new()
+    {
+        public static T FactoryMethod()
+        {
+            return new T();
+        }
+    }
+}
